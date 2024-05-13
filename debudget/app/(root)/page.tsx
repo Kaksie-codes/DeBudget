e18f7cf2cea32d99e45a9f35c13e6f5823ced488
Nsikak Akpan <builtbykaksy@gmail.com>
2024-05-13 08:52:00 +0100

    Implement complete layout

--- debudget/app/(root)/page.tsx
+++ debudget/app/(root)/page.tsx
@@ -1,8 +1,39 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {
+        firstName: "Nsikak", 
+        lastName: 'Akpan', 
+        email: 'nsikakakpan007@gmail.com'
+    }
+
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox
+                    type="greeting"
+                    title="welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext='Access and mangae your Account and transactions efficiently.'
+                />     
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                />           
+            </header>
+            RECENT TRANSITIONS
+        </div>
+        <RightSidebar 
+            user={loggedIn}
+            transactions={[]}
+            banks={[]}
+        />
+    </section>
   )
 }
 
